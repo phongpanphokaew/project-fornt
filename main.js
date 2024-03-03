3c6e6d52f23a276a80eda70990ce380e3f626451
phongpanphokaew <s6206021631029@emailkmutnb.ac.th>
2024-03-03 21:14:52 +0700

    api url update

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
 
-const base_url = "http://localhost:3000/api";
-
+// const base_url = "http://localhost:3000/api";
+const base_url = "http://node59699-back-end.proen.app.ruk-com.cloud:11948/api";
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
