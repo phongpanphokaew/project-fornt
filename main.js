24e784a79e64c4d77da6d19edcd175e5642ba315
phongpanphokaew <s6206021631029@emailkmutnb.ac.th>
2024-05-22 23:43:20 +0700

    up

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = express();
 var bodyParser = require('body-parser');
 
 //const base_url = "http://localhost:3000/api";
-const base_url = "https://project.proen.app.ruk-com.cloud//api";
+const base_url = "https://project.proen.app.ruk-com.cloud/api";
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
